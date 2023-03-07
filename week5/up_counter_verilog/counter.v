c395ba35d9bfa7ee4a5884ff07d3ba348f7c057b
Pramit Kumar Pal <pramitpalkumar@gmail.com>
2023-03-07 13:56:28 +0000

    edited up_counter verilog for en pin

--- week5/up_counter_verilog/counter.v
+++ week5/up_counter_verilog/counter.v
@@ -4,7 +4,7 @@ module up_counter(input clk, reset,en, output[3:0] counter,countern
 	reg [3:0] counter_up;
 
 // up counter
-  always @(posedge clk or posedge reset or posedge en) begin
+  always @(posedge clk or posedge reset) begin
 		if(reset)
  			counter_up <= 4'd0;
     	else if(en)
